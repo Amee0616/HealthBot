8d36afcd04784011ea29df9b504e56960db33fe6
AmeePatel <107007773+Amee0616@users.noreply.github.com>
2024-06-12 12:46:16 -0400

    simple-app.py

--- simple-app.py
+++ simple-app.py
@@ -3,10 +3,21 @@ from langchain_voyageai import VoyageAIEmbeddings
 import os
 import boto3
 from urllib.parse import urlparse
+from pinecone import Pinecone
 import pinecone
 from langchain_openai import ChatOpenAI
+import openai
+from langchain.chains import LLMChain, RetrievalQA
+import time
+import re
+from langchain_pinecone import PineconeVectorStore
+from langchain.memory import ConversationBufferMemory
 from langchain.schema import HumanMessage
 from langchain.prompts import ChatPromptTemplate
+from langchain.chains import ConversationChain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import uuid
 import warnings
 
 # Ignore all warnings
@@ -56,7 +67,16 @@ def retrieve_and_format_response(query, retriever, llm, chat_history):
     message = HumanMessage(content=prompt)
 
     response = llm([message])
-    return response.content  # Ensure returning the response content as string
+    return response
+
+# Function to save chat history to a file
+def save_chat_history_to_file(filename, history):
+    with open(filename, 'w') as file:
+        file.write(history)
+
+# Function to upload the file to S3
+def upload_file_to_s3(bucket, key, filename):
+    s3_client.upload_file(filename, bucket, key)
 
 # Setup - Streamlit secrets
 OPENAI_API_KEY = st.secrets["api_keys"]["OPENAI_API_KEY"]
@@ -69,15 +89,18 @@ aws_region = st.secrets["aws"]["aws_region"]
 # Langchain stuff
 llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
 
+# Initialize the conversation memory
+memory = ConversationBufferMemory()
+
 prompt_template = ChatPromptTemplate.from_template(
-    "Instruction: You are a helpful assistant to help users with their patient education queries. \
-    Based on the following information, provide a summarized & concise explanation using a couple of sentences. \
-    Only respond with the information relevant to the user query {query}, \
-    if there are none, make sure you say the `magic words`: 'I don't know, I did not find the relevant data in the knowledge base.' \
-    But you could carry out some conversations with the user to make them feel welcomed and comfortable, in that case you don't have to say the `magic words`. \
-    In the event that there's relevant info, make sure to attach the download button at the very end: \n\n[More Info]({s3_gen_url}) \
-    Context: {combined_content}"
-)
+        "Instruction: You are a helpful assistant to help users with their patient education queries. \
+        Based on the following information, provide a summarized & concise explanation using a couple of sentences. \
+        Only respond with the information relevant to the user query {query}, \
+        if there are none, make sure you say the `magic words`: 'I don't know, I did not find the relevant data in the knowledge base.' \
+        But you could carry out some conversations with the user to make them feel welcomed and comfortable, in that case you don't have to say the `magic words`. \
+        In the event that there's relevant info, make sure to attach the download button at the very end: \n\n[More Info]({s3_gen_url}) \
+        Context: {combined_content}"
+    )
 
 # Initialize necessary objects (s3 client, Pinecone, OpenAI, etc.)
 s3_client = boto3.client(
@@ -86,18 +109,17 @@ s3_client = boto3.client(
     aws_secret_access_key=aws_secret_access_key,
     region_name=aws_region
 )
-
 # PINECONE
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-pinecone.init(api_key=PINECONE_API_KEY)
+pc = Pinecone(api_key=PINECONE_API_KEY)
 index_name = "test"
 openai.api_key = OPENAI_API_KEY
 
 # Set up LangChain objects
 # VOYAGE AI
-model_name = "voyage-large-2"
+model_name = "voyage-large-2"  
 embedding_function = VoyageAIEmbeddings(
-    model=model_name,
+    model=model_name,  
     voyage_api_key=VOYAGE_AI_API_KEY
 )
 # Initialize the Pinecone client
@@ -107,6 +129,13 @@ vector_store = PineconeVectorStore.from_existing_index(
 )
 retriever = vector_store.as_retriever()
 
+# Initialize rag_chain
+rag_chain = (
+    {"retrieved_context": retriever, "question": RunnablePassthrough()}
+    | prompt_template
+    | llm
+)
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
@@ -114,8 +143,8 @@ if "messages" not in st.session_state:
 # Sidebar for chat history
 st.sidebar.title("Chat History")
 for i, message in enumerate(st.session_state["messages"]):
-    role = "User" if message["role"] == "user" else "Assistant"
-    st.sidebar.write(f"{role} {i+1}: {message['content']}")
+    with st.sidebar.expander(f"Message {i+1} - {message['role']}"):
+        st.write(message["content"])
 
 # Display chat messages from history
 for message in st.session_state["messages"]:
@@ -138,7 +167,7 @@ if user_input:
         # Compile the chat history
         chat_history = "\n".join([f"{msg['role']}: {msg['content']}" for msg in st.session_state["messages"]])
         
-        bot_response = retrieve_and_format_response(user_input, retriever, llm, chat_history)
+        bot_response = retrieve_and_format_response(user_input, retriever, llm, chat_history).content
     
     st.session_state["messages"].append({"role": "assistant", "content": bot_response})
     
